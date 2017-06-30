e9ec0f979796e07a032ee5a9d4dac6264610d23e
RayLiu-1 <ruli7792@colorado.edu>
2017-06-30 06:36:58 -0600

    a

--- server.c
+++ server.c
@@ -133,7 +133,7 @@ void *recerving_handler(void *pfd) {
 					newNode->next = NULL;
 					memset(newNode->site, 0, BUFFERSIZE);
 					strncpy(newNode->site, host, strlen(host));
-					QueueAdd(&requests, newNode);
+					//QueueAdd(&requests, newNode);
 					//printf("%d\n", requests.size);
 					fprintf(fp, "%s\n  00   00   00   IN_QUEUE   \n",host);
 					host = strtok(NULL, ",\n ");
