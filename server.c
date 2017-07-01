3cbdded6d895d000e8c9479ac41d26f665f5987c
RayLiu-1 <ruli7792@colorado.edu>
2017-06-30 23:46:43 -0600

    w

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 #include <sys/time.h>   
-//#include <arpa/inet.h>
+#include <arpa/inet.h>
 #include <unistd.h>
 #include <sys/fcntl.h>
 #include <sys/types.h> 
