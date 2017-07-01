9bc9b3eff4955c472843178b05a0bff276921f85
RayLiu-1 <ruli7792@colorado.edu>
2017-07-01 08:09:54 -0600

    www

--- server.c
+++ server.c
@@ -122,7 +122,7 @@ void *recerving_handler(void *pfd) {
 					memset(newNode->site, 0, BUFFERSIZE);
 					strncpy(newNode->site, host, strlen(host));
 					QueueAdd(&requests, newNode);
-					host = strtok(NULL, ",\n ");
+					host = strtok(NULL, " ,\n ");
 				}
 			}
 			else {
@@ -140,8 +140,9 @@ void *recerving_handler(void *pfd) {
 			char *handleA;
 			int handleI = max_handle+1;
 			handleA = strtok(request, " \n");
-			handleA = strtok(NULL, " \n");
+			handleA = strtok(NULL, " ,\n");
 			char handle_msg[BUFFERSIZE];
+			
 			//printf("%s\n",handleA);
 			if (handleA!=NULL) {
 				handleI = atoi(handleA);
